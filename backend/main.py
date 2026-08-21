3034d9abd36093ea720de8eb0cf2eeff55105426
Vishwateja Pothula <vishwapothula23@gmail.com>
2026-08-21 16:42:14 +0530

    Update allowed origins for CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://intern-hub-ashen.vercel.app",
+        "https://internhub-peach.vercel.app",
     ],
 
     allow_credentials=True,
